706a8736ccb584c9db9e84a7dee3e30d00fcaa9f
Micah Fischer <mfischer981@west-mec.org>
2019-08-27 16:07:26 -0700

    Learning the difference of let and const
    Learning the difference of let and const.

--- sub-unit-1/02_let_const/app.js
+++ sub-unit-1/02_let_const/app.js
@@ -9,13 +9,43 @@ const testVar = (params) => {
     console.log("Function Logged:" + a);
     
     if (true) {
-        var a = 50;
+        a = 50;
         console.log("Block Logged:" + a);
     }
     console.log("Here:" + a);
 
 }
 
+const pizza = [
+    {
+        name: "cheese",
+        number: 1,
+        price: 1
+    }, 
+    {
+        name: "mushroom",
+        number: 2,
+        price: 1
+    },
+    {
+        name: "pineapple",
+        number: 3,
+        price: 1
+    }
+]
+
 a => a += a;
 testVar();
 console.log("End Logged:" + a);
+
+var pizzaObj = {
+    pepp: "monday",
+    array: [
+        1,
+        2,
+        3
+    ],
+    funFunction (planePeople) {
+        console.log(`hi ${planePeople} outside my window!`)
+    }
+}
